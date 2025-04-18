7013d86dffdf5691f58099259e2cd05dfdb11873
Abdulrahman Mashaal <abdul.attiamashaal@gmail.com>
2025-04-18 22:50:25 +0200

    What are RxJs Operators? Learn the Map Operator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
+import { createHttpObservable } from '../common/util';
+import { noop } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Component({
     selector: 'about',
     templateUrl: './about.component.html',
@@ -11,6 +13,15 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const http$ = createHttpObservable('/api/courses');
+    const courses$ = http$
+    .pipe(
+      map(res => Object.values(res.payload))
+    );
+    courses$.subscribe(
+      courses => console.log(courses),
+      noop, // noop is a function that does nothing
+      () => console.log('Completed') // empty function
+    )
   }
-
 }
