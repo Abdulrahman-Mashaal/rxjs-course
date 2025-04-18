ded4d746252693f0e9325cc17aa2bc008f35a9c9
Abdulrahman Mashaal <abdul.attiamashaal@gmail.com>
2025-04-18 23:08:24 +0200

    11.Building Components with RxJs - Imperative Design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
 import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-
+import {catchError, delayWhen, filter, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import { noop } from 'rxjs';
+import { createHttpObservable } from '../common/util';
 
 @Component({
     selector: 'home',
@@ -12,14 +13,28 @@ import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/oper
 })
 export class HomeComponent implements OnInit {
 
-
+    beginnerCourses: Course[];
+    advancedCourses: Course[];
     constructor() {
-
+  
     }
 
     ngOnInit() {
+  
 
-
+        const http$ = createHttpObservable('/api/courses');
+        const courses$ = http$
+        .pipe(
+          map(res => Object.values(res.payload) as any[])
+        );
+        courses$.subscribe(
+          courses => {
+            this.beginnerCourses = courses.filter(course => course.category === 'BEGINNER');
+            this.advancedCourses = courses.filter(course => course.category === 'ADVANCED');
+          },
+          noop, // noop is a function that does nothing
+          () => console.log('Completed') // empty function
+        )
 
     }
 
