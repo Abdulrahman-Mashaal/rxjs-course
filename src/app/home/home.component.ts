65e0a3765b9b864e55f6291533212aaba47e6e25
Abdulrahman Mashaal <abdul.attiamashaal@gmail.com>
2025-04-19 13:01:45 +0200

    12. Building Components with RxJs - Reactive Design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,41 +1,44 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, filter, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-import { noop } from 'rxjs';
-import { createHttpObservable } from '../common/util';
+import { Component, OnInit } from "@angular/core";
+import { Course } from "../model/course";
+import { interval, Observable, of, timer } from "rxjs";
+import {
+  catchError,
+  delayWhen,
+  filter,
+  map,
+  retryWhen,
+  shareReplay,
+  tap,
+} from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css'],
-    standalone: false
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+  standalone: false,
 })
 export class HomeComponent implements OnInit {
+  beginnerCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
+  constructor() {}
 
-    beginnerCourses: Course[];
-    advancedCourses: Course[];
-    constructor() {
-  
-    }
-
-    ngOnInit() {
-  
-
-        const http$ = createHttpObservable('/api/courses');
-        const courses$ = http$
-        .pipe(
-          map(res => Object.values(res.payload) as any[])
-        );
-        courses$.subscribe(
-          courses => {
-            this.beginnerCourses = courses.filter(course => course.category === 'BEGINNER');
-            this.advancedCourses = courses.filter(course => course.category === 'ADVANCED');
-          },
-          noop, // noop is a function that does nothing
-          () => console.log('Completed') // empty function
-        )
-
-    }
-
+  ngOnInit() {
+    const http$: Observable<Course[]> = createHttpObservable("/api/courses");
+    const courses$: Observable<Course[]> = http$.pipe(
+      map((res) => Object.values(res["payload"] as Course[]))
+    );
+    // Observable definition
+    this.beginnerCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category === "BEGINNER")
+      )
+    );
+    // Observable definition
+    this.advancedCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category === "ADVANCED")
+      )
+    );
+  }
 }
